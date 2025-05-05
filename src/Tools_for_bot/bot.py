2292644e4c39f31dd4e4024863a044d010f7af71
Dmitrii <dgorbacev742@gmail.com>
2025-05-05 20:54:48 +0300

    add commandStarts instead command('start'), add templates.py

--- src/Tools_for_bot/bot.py
+++ src/Tools_for_bot/bot.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandStart
 from aiogram.fsm.storage.memory import MemoryStorage
+from src.Configs.templates import hello_message, help_message
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,17 +12,21 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
-@dp.message(Command("start"))
+
+@dp.message(CommandStart())
 async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я твой Telegram бот. Чтобы зарегистрироваться, используй команду /register")
+    await message.reply(hello_message)
+
 
 @dp.message(Command("help"))
 async def help_command(message: types.Message):
-    await message.reply("Доступные команды:\n/start - начать работу с ботом\n/register - регистрация в системе")
+    await message.reply(help_message)
+
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
